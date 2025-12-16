697249abcdf631a450b00aab809817e05114b0d8
Anish-Karkee <anitakarkey7@gmail.com>
2025-12-16 21:42:54 +0545

    Initial push

--- recall2/src/constant/productData.js
+++ recall2/src/constant/productData.js
@@ -1,20 +1,20 @@
 import apple from "../assets/images/rolex.jpg"
 import rolex from "../assets/images/rolex.jpg"
-import watch from "../assets/images/rolex.jpg"
+import rolex0 from "../assets/images/rolex0.png"
 
 export const productData = [
 {name:"Smart Watch",brand:"Apple",price:1000,img:apple},
 {name:"White Phantom",brand:"Rolex",price:12000,img:rolex},
-{name:"Analog Watch",brand:"BENYAR",price:1500,img:watch},
+{name:"Analog Watch",brand:"BENYAR",price:1500,img:rolex0},
 {name:"Smart Watch",brand:"Apple",price:1000,img:apple, },
 {name:"White Phantom",brand:"Rolex",price:12000,img:rolex},
-{name:"Analog Watch",brand:"BENYAR",price:1500,img:watch},
+{name:"Analog Watch",brand:"BENYAR",price:1500,img:rolex0},
 {name:"Smart Watch",brand:"Apple",price:1000,img:apple},
 {name:"White Phantom",brand:"Rolex",price:12000,img:rolex},
-{name:"Analog Watch",brand:"BENYAR",price:1500,img:watch},
+{name:"Analog Watch",brand:"BENYAR",price:1500,img:rolex0},
 {name:"Smart Watch",brand:"Apple",price:1000,img:apple},
 {name:"White Phantom",brand:"Rolex",price:12000,img:rolex},
-{name:"Analog Watch",brand:"BENYAR",price:1500,img:watch},
+{name:"Analog Watch",brand:"BENYAR",price:1500,img:rolex0},
 
 
 ]
